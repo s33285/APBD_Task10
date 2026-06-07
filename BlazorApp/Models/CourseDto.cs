4aa803880c90a969f691fe609118632399a008f3
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-06-07 17:54:32 +0200

    Created Models for blazor

---
+++ BlazorApp/Models/CourseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Models
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Ects { get; set; }
+    }
+}
---
+++ BlazorApp/Models/StudentDto.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorApp.Models
+{
+    public class StudentDto
+    {
+    }
+}
